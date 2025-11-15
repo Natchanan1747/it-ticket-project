59dfabd9bfa9cde307f0b70bebd2da1e45d27cf3
Natchanan1747 <66160023@go.buu.ac.th>
2025-11-16 04:52:25 +0700

    แก้ไข allowroles

--- backend/src/routes/ticketRoute.js
+++ backend/src/routes/ticketRoute.js
@@ -9,6 +9,6 @@ ticketRouter.post("/", authMiddleware, allowRoles("user"), upload.single("attach
 ticketRouter.get("/", authMiddleware, allowRoles("admin", "staff"),ticketController.getAllTickets);
 ticketRouter.get("/:ticketId", authMiddleware, allowRoles("admin", "staff", "user"),ticketController.getTicketById);
 ticketRouter.post("/:ticketId/comments", authMiddleware, allowRoles("staff","user"), ticketController.createComment);
-ticketRouter.post("/:ticketId/status", authMiddleware, allowRoles("staff", "admin"), ticketController.updateStatus);
+ticketRouter.post("/:ticketId/status", authMiddleware, allowRoles("admin", "staff"), ticketController.updateStatus);
 
 export default ticketRouter;
