59d44597ffb9f7fff67c8252a249515f6466fffa
Natchanan1747 <66160023@go.buu.ac.th>
2025-11-16 04:20:44 +0700

    edit ticketRoutes

--- backend/src/routes/ticketRoute.js
+++ backend/src/routes/ticketRoute.js
@@ -4,16 +4,11 @@ import upload from "../utils/upload.js";
 import { authMiddleware, allowRoles } from "../middlewares/authMiddleware.js";
 const ticketRouter = Router()
 
-ticketRouter.get(
-  "/my-tickets", 
-  authMiddleware, 
-  ticketController.getMyTickets
-);
-
+ticketRouter.get("/my-tickets", authMiddleware, ticketController.getMyTickets);
 ticketRouter.post("/", authMiddleware, allowRoles("user"), upload.single("attachments"), ticketController.createTicket);
 ticketRouter.get("/", authMiddleware, allowRoles("staff, admin"),ticketController.getAllTickets);
 ticketRouter.get("/:ticketId", authMiddleware, allowRoles("staff","admin", "user"),ticketController.getTicketById);
 ticketRouter.post("/:ticketId/comments", authMiddleware, allowRoles("staff","user"), ticketController.createComment);
-ticketRouter.post("/:ticketId/status", authMiddleware, allowRoles("staff"), ticketController.updateStatus);
+ticketRouter.post("/:ticketId/status", authMiddleware, allowRoles("staff", "admin"), ticketController.updateStatus);
 
 export default ticketRouter;
